2fb275e0872f273f2b6f0e20dd5c04a7dc1dd594
ishaaggarwal <isha.aggarwal@joshworkz.com>
2014-01-14 16:55:13 +0530

    Bug fixed

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,7 @@
 class Tweet < ActiveRecord::Base
   # attr_accessible :title, :body
     belongs_to :admin
-    validates_length_of :content , :minimum => 1 , :maximum => 10 , :message => "Tweet too long..."
+    validates_length_of :content , :minimum => 1 , :maximum => 1000 , :message => "Tweet too long..."
     default_scope -> { order('created_at DESC') }
     attr_accessible :admin_id , :content
 end
